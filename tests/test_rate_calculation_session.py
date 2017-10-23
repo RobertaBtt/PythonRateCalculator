9a800785deb932b1276295b8bf74a5f383aaad84
RobertaBtt <roberta.btt@gmail.com>
2017-10-23 08:51:53 +0200

    Tests

--- tests/test_rate_calculation_session.py
+++ tests/test_rate_calculation_session.py
@@ -10,13 +10,17 @@ class TestRateCalculationSession(unittest.TestCase):
     def setUp(self):
         self.csv_file_path = 'data.csv'
         self.rate_calculation_session = RateCalculationSession.RateCalculationSession()
+        self.offers = CsvParser.CsvParser.get_rows(self.csv_file_path)
 
     def test_isnot_amount_available(self):
-        offers = CsvParser.CsvParser.get_rows(self.csv_file_path)
         loan_amount = 10000
-        self.assertEqual(False, self.rate_calculation_session.is_loan_possible( offers, loan_amount))
+        self.assertEqual(False, self.rate_calculation_session.is_loan_possible(self.offers, loan_amount))
 
     def test_is_amount_available(self):
-        offers = CsvParser.CsvParser.get_rows(self.csv_file_path)
         loan_amount = 1000
-        self.assertEqual(True, self.rate_calculation_session.is_loan_possible( offers, loan_amount))
+        self.assertEqual(True, self.rate_calculation_session.is_loan_possible(self.offers, loan_amount))
+
+    def test_get_rates(self):
+        loan_amount = 1000
+        self.assertEqual(True, self.rate_calculation_session.get_rates(self.offers, loan_amount))
+
