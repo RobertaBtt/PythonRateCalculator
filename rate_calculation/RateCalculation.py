d647deac4262f797b359fc07db6b571c3e1d0612
roberta.buttau <roberta.buttau@enermed.it>
2017-10-23 16:01:13 +0200

    Finalizing and tests and running in console test

--- rate_calculation/RateCalculation.py
+++ rate_calculation/RateCalculation.py
@@ -29,18 +29,19 @@ class RateCalculation():
         amount_reached = 0.0
 
         rates = []
-        for index, offer in enumerate(offers):
+        if loan_amount>0:
+            for index, offer in enumerate(offers):
 
-            amount_reached += float(offer[1])
-            if amount_reached < loan_amount:
-                rates.append((float(offer[0]), float(offer[1])))
+                amount_reached += float(offer[1])
+                if amount_reached < loan_amount:
+                    rates.append((float(offer[0]), float(offer[1])))
 
-            elif amount_reached == loan_amount:
-                rates.append((float(offer[0]), float(offer[1])))
-                return rates
-            elif amount_reached > loan_amount:
-                rates.append((float(offer[0]), float(offer[1]) - (amount_reached - loan_amount)))
-                return rates
+                elif amount_reached == loan_amount:
+                    rates.append((float(offer[0]), float(offer[1])))
+                    return rates
+                elif amount_reached > loan_amount:
+                    rates.append((float(offer[0]), float(offer[1]) - (amount_reached - loan_amount)))
+                    return rates
 
         return rates
 
@@ -49,20 +50,25 @@ class RateCalculation():
         :param rates_list: a tuple containing all the taxes
         :return: the avg of the taxes
         """
+        if len(rates_list) > 0:
+            avg_rates = sum([v[0] for v in rates_list]) / float(len(rates_list))
+            return round(avg_rates, 2)
 
-        avg_rates = sum([v[0] for v in rates_list]) / float(len(rates_list))
-        return round(avg_rates, 2)
+        return 0
 
-    def get_annual_repayment(self, num_units_per_year, loan_amount, tax_rate, total_of_units):
+    def get_repayment(self, num_units_per_year, loan_amount, tax_rate, total_of_units):
         """
         :param num_units_per_year: how many payment in a year
         :param loan_amount:  what is the amout of the requested load
         :param tax_rate: the tax rate applied
         :param total_of_units: total of number of payments
-        :return:
+        :return: the payment for unit
         """
 
-        tax_rate_per_units = tax_rate/num_units_per_year
+        if (num_units_per_year > 0 and loan_amount > 0 and tax_rate > 0 and total_of_units > 0):
 
-        payment = ( tax_rate_per_units * loan_amount) / (1-(1+tax_rate_per_units)**(-total_of_units))
-        return payment
+            tax_rate_per_units = tax_rate/num_units_per_year
+            payment = ( tax_rate_per_units * loan_amount) / (1-(1+tax_rate_per_units)**(-total_of_units))
+            return payment
+
+        return 0
